84192d4363526673c577ce67029e7541efdb94e6
須田直文 <developsuda@gmail.com>
2024-11-03 06:12:30 +0000

    Add data cleanup and error handling in migration script

--- migrations/migration.go
+++ migrations/migration.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"pokematch/infra"
 	"pokematch/models"
 )
@@ -11,5 +12,14 @@ func main() {
 
 	if err := db.AutoMigrate(&models.Pokemon{}); err != nil {
 		panic("Failed to migrate database")
+	}
+
+	// テーブルの中身を全て削除
+	db.Unscoped().Where("1=1").Delete(&models.Pokemon{})
+	list := models.ReturnList()
+	result := db.Create(&list)
+
+	if result.Error != nil {
+		fmt.Println(result.Error)
 	}
 }
