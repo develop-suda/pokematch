1db9eb24d0cb2214f88eca56fc80ee14d05b12df
develop-suda <developsuda@gmail.com>
2024-11-24 04:24:52 +0000

    Update Pokemon model to specify numeric precision for height and weight

--- models/pokemon.go
+++ models/pokemon.go
@@ -4,7 +4,8 @@ import "gorm.io/gorm"
 
 type Pokemon struct {
 	gorm.Model
-	Name   string  `gorm:"not null"`
-	Height float32 `gorm:"not null"`
-	Weight float32 `gorm:"not null"`
+	Name string `gorm:"not null"`
+	// 最大精度と最大位取りを設定
+	Height float32 `gorm:"type:numeric(3,1);not null"`
+	Weight float32 `gorm:"type:numeric(4,1);not null"`
 }
