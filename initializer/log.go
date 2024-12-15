e4947c35c95a0bc1f55892ad1280c7b914870d20
develop-suda <developsuda@gmail.com>
2024-12-15 02:24:34 +0000

    Update logging configuration to create date-based log files for gin and zerolog

--- initializer/log.go
+++ initializer/log.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"io"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/gin-contrib/requestid"
 	"github.com/gin-gonic/gin"
@@ -11,17 +13,22 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-// TODO: 日付ごとにログファイルを分ける
-// TODO: ログ用のフォルダを作成する
 // TODO: 各処理のログを出力する
 func Log() {
 	// gin.log,go.logファイル作成
-	ginf, _ := os.Create("gin.log")
-	gof, _ := os.Create("go.log")
+	ginf, err := getLogFile("gin")
+	if err != nil {
+		log.Error().Msg("error")
+	}
+
+	zerologf, err := getLogFile("zerolog")
+	if err != nil {
+		log.Error().Msg("error")
+	}
 	// ginでのログをファイルと標準出力に出力
 	gin.DefaultWriter = io.MultiWriter(ginf, os.Stdout)
 	// zerologでのログをファイルと標準出力に出力
-	log.Logger = log.Output(io.MultiWriter(gof, os.Stdout))
+	log.Logger = log.Output(io.MultiWriter(zerologf, os.Stdout))
 }
 
 func CreateLogger(c *gin.Context) *zerolog.Logger {
@@ -41,4 +48,33 @@ func CreateLogger(c *gin.Context) *zerolog.Logger {
 	// context.Contextに設定したロガーを取り出し
 	newLogger := zerolog.Ctx(ctx)
 	return newLogger
+}
+
+func getLogFile(fileName string) (*os.File, error) {
+	t := time.Now()
+
+	y := strconv.Itoa(t.Year())
+	m := strconv.Itoa(int(t.Month()))
+	d := strconv.Itoa(t.Day())
+
+	yyyymmdd := y + m + d
+
+	filePath := "logs/" + fileName + "/" + yyyymmdd + "_" + fileName + ".log"
+
+	// ファイルが存在しない場合　err != nil
+	// ファイルが存在する場合　　　　err = nil
+	_, err := os.Stat(filePath)
+	if err != nil {
+		return createLogFile(filePath)
+	} else {
+		return openLogFile(filePath)
+	}
+}
+
+func createLogFile(filePath string) (*os.File, error) {
+	return os.Create(filePath)
+}
+
+func openLogFile(filePath string) (*os.File, error) {
+	return os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 }
